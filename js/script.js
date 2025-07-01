794d5c05ef4fb9c200ce395e3f0abe51c3ca4b8a
Gilang Nugroho <gilangnugroho1103@gmail.com>
2025-07-01 12:36:27 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -12,22 +12,22 @@ function switchMode(allMode) {
   footer.classList.toggle("dark");
   body.classList.toggle("dark");
 
-  // Ganti ikon dark/light
+  // Ganti ikon dark/light pakai path RELATIF
   const isDark = body.classList.contains("dark");
   darkMode.src = isDark
-    ? "/img/light-mode-svgrepo-com.svg"
-    : "/img/mode-dark-svgrepo-com.svg";
+    ? "./img/light-mode-svgrepo-com.svg"
+    : "./img/mode-dark-svgrepo-com.svg";
   darkMode.alt = isDark ? "light" : "dark";
 
   // ganti ikon webLink
   document.querySelectorAll(".webLink").forEach(function (switchWebLink) {
     switchWebLink.src = isDark
-      ? "/img/web-link-white-svgrepo-com.svg"
-      : "/img/web-link-svgrepo-com.svg";
+      ? "./img/web-link-white-svgrepo-com.svg"
+      : "./img/web-link-svgrepo-com.svg";
   });
 
   // ganti ikon college
   collegeIcon.src = isDark
-    ? "/img/college-svgrepo-com (white).svg"
-    : "/img/college-svgrepo-com (birdonk).svg";
+    ? "./img/college-svgrepo-com (white).svg"
+    : "./img/college-svgrepo-com (birdonk).svg";
 }
